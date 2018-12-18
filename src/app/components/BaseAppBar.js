ca0c4b8dd8e94d8323f21c98eafc9ab6242e5d89
Tzu-Yin Hong <lili668668@gmail.com>
2018-12-18 14:25:17 +0800

    Add buttons

--- src/app/components/BaseAppBar.js
+++ src/app/components/BaseAppBar.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next/hooks'
 import { makeStyles } from '@material-ui/styles'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import CodeIcon from '@material-ui/icons/Code'
+import info from '../../info'
 import AppBar from './AppBar'
 import ToolbarIconButton from '../plugins/ToolbarIconButton'
 
@@ -12,10 +15,14 @@ const useStyles = makeStyles(theme => ({
 
 function BaseAppBar (props) {
   const classes = useStyles()
+  const [t] = useTranslation()
   return (
     <AppBar>
       <div className={classes.grow} />
-      <ToolbarIconButton icon={AccountCircleIcon} tooltip='簡介' onClick={() => {}} />
+      <ToolbarIconButton icon={AccountCircleIcon} tooltip={t('introduction')} onClick={() => {}} />
+      <ToolbarIconButton icon={CodeIcon} tooltip={t('GitHub Repo')} onClick={() => {
+        window.location = info.repo
+      }} />
     </AppBar>
   )
 }
