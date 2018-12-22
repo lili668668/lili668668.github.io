2d3615256b93a7b9ca042392263ae859237ea0d6
Tzu-Yin Hong <lili668668@gmail.com>
2018-12-22 13:15:05 +0800

    Update version 0.1.1

---
+++ precache-manifest.6f0cbf5930bfa61f88d7cc6825245d4b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "843705ecdcb7c86769ab",
+    "url": "/static/js/runtime~main.843705ec.js"
+  },
+  {
+    "revision": "cde7e531ba706169eb7a",
+    "url": "/static/js/main.cde7e531.chunk.js"
+  },
+  {
+    "revision": "c77dfb4b882a2db91e90",
+    "url": "/static/js/5.c77dfb4b.chunk.js"
+  },
+  {
+    "revision": "372f6c649773fcc5668a",
+    "url": "/static/js/4.372f6c64.chunk.js"
+  },
+  {
+    "revision": "d0eaca4190aa419bb49a",
+    "url": "/static/js/3.d0eaca41.chunk.js"
+  },
+  {
+    "revision": "07d6d5541afcce2edb79",
+    "url": "/static/js/2.07d6d554.chunk.js"
+  },
+  {
+    "revision": "de8327e721fe90bcc7e7",
+    "url": "/static/js/0.de8327e7.chunk.js"
+  },
+  {
+    "revision": "7685c9ec9f81d131d11b7fbb26964c71",
+    "url": "/index.html"
+  }
+];
