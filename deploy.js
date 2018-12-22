649f6134ea19f6c733391765da928b88713fe765
Tzu-Yin Hong <lili668668@gmail.com>
2018-12-22 13:13:59 +0800

    Fix deploy problem

--- deploy.js
+++ deploy.js
@@ -2,9 +2,10 @@ const ghpages = require('gh-pages')
 const config = require('./package.json')
 
 ghpages.publish('build', {
-  src: 'CNAME',
   branch: 'master',
   repo: 'https://github.com/lili668668/lili668668.github.io.git',
   message: `Update version ${config.version}`
-}, (err) => console.log(err))
+}, (err) => {
+  if (err) console.log(err)
+})
 
