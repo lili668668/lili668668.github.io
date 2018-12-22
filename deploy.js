3feaa807b49d598c2fe375f1c14f2739a7a771d4
Tzu-Yin Hong <lili668668@gmail.com>
2018-12-22 13:00:07 +0800

    Add CNAME

--- deploy.js
+++ deploy.js
@@ -1,7 +1,10 @@
 const ghpages = require('gh-pages')
+const config = require('./package.json')
 
 ghpages.publish('build', {
+  src: 'CNAME',
   branch: 'master',
-  repo: 'https://github.com/lili668668/lili668668.github.io.git'
+  repo: 'https://github.com/lili668668/lili668668.github.io.git',
+  message: `Update version ${config.version}`
 })
 
