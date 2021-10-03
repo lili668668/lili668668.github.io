2f1749564d12583bec8d0b8e6c4ddf66b63d080c
Tzu-Yin Hong <lili668668@gmail.com>
2021-10-03 14:02:41 +0800

    fix: fix gihub auth problem

--- deploy.js
+++ deploy.js
@@ -3,7 +3,7 @@ const config = require('./package.json')
 
 ghpages.publish('build', {
   branch: 'master',
-  repo: `https://github.com/${config.github}/${config.github}.github.io.git`,
+  repo: `git@github.com:${config.github}/${config.github}.github.io.git`,
   message: `Update version ${config.version}`
 }, (err) => {
   if (err) console.log(err)
